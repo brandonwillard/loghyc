ee1fbc4fe57b216dda19639cff8d4ef42cd71ce0
Gergely Nagy <algernon@madhouse-project.org>
2014-01-07 20:35:55 +0100

    setup.py: New file
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

---
+++ setup.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+## adderall - miniKanren in Hy
+## Copyright (C) 2014  Gergely Nagy <algernon@madhouse-project.org>
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import find_packages, setup
+
+setup(
+    name="adderall",
+    version="0.0.0",
+    install_requires = ['hy>=0.9.12'],
+    packages=find_packages(exclude=['tests', 'tests.schemer']),
+    package_data={
+        'adderall': ['*.hy'],
+    },
+    author="Gergely Nagy",
+    author_email="algernon@madhouse-project.org",
+    long_description="""a miniKanren implementation in Hy.""",
+    license="GPL-3",
+    url="https://github.com/algernon/adderall",
+    platforms=['any'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: DFSG approved",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Lisp",
+        "Topic :: Software Development :: Libraries",
+    ]
+)
