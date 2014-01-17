3c5135c77fd5492d52b9972bab83dd34789cb17d
Gergely Nagy <algernon@balabit.hu>
2014-01-17 15:42:45 +0100

    tests/bench.py: Better result display
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

--- tests/bench.py
+++ tests/bench.py
@@ -22,12 +22,13 @@ import sys
 SETUP_FORMAT="""import hy
 from {0} import {1}"""
 
-def benchmark(module, function, times = 25):
+def benchmark(module, function, times = 10):
     print ("{0}/{1} * {2}:".format(module, function, times)),
     sys.__stdout__.flush()
-    print (timeit.timeit("{0}()".format(function),
-                         setup = SETUP_FORMAT.format(module, function),
-                         number = times))
+    result = timeit.timeit("{0}()".format(function),
+                           setup = SETUP_FORMAT.format(module, function),
+                           number = times)
+    print ("{0:.5f}s ({1:.5f}s average)".format(result, result / times))
 
 
 if __name__ == '__main__':
