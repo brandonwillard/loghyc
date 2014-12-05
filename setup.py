3171509875541fdcab82ee435c03062ec83af238
Gergely Nagy <algernon@madhouse-project.org>
2014-12-05 14:18:24 +0100

    Release adderall 0.1.3
    Added a short summary of changes to NEWS, and upped the version in
    setup.py.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="adderall",
-    version="0.1.2",
+    version="0.1.3",
     install_requires = ['hy>=0.10', 'monaxhyd>=0.1.0'],
     packages=find_packages(exclude=['tests', 'tests.extra', 'tests.schemer']),
     package_data={
